f5e881f313e023ce2b8bf54e71f8354a041b6f61
Felipe Velandia <fvelandia695@gmail.com>
2019-09-06 09:31:49 -0500

    Add files via upload

---
+++ busqueda.py
@@ -0,0 +1,102 @@
+import random
+
+print("Este programa encuentra un número de un vector")
+print("Por medio del Método de Divide y vencerás")
+print
+
+def binaria (vec, izq, der, llave, c, cont):
+    cont+=1
+    c[0] += 1
+    c[cont] +=1
+    
+    if izq > der:
+        c[0] += 1
+        c[cont] += 1
+        return None
+    
+    c[0] += 4
+    c[cont] += 4
+    mit = (der+izq)//2
+    
+    c[0] += 2
+    c[cont] += 2
+    
+    if vec[mit] == llave:
+        c[0] += 1
+        c[cont] += 1
+        return mit
+    elif vec[mit] > llave:
+        binaria(vec, izq, mit-1, llave, c, cont)
+    else:
+        binaria(vec, mit+1, der, llave, c, cont)
+        
+    c[0] += 2
+    c[cont] += 2
+    
+def burbuja (vec, i):
+    j=0
+    while j < i-1:
+        k=j+1
+        while k < i:
+            if vec[j] > vec[k]:
+                t = vec[j]
+                vec[j] = vec[k]
+                vec[k] = t
+            k=k+1
+        j=j+1
+    return vec
+
+i=10
+cont = 0
+x = range(10)
+y = range(10)
+c = list(range(11))
+
+while i <= 10:
+    vec = range(i)
+    x[cont] = i
+    
+    for j in range(i):
+        vec[j] = random.randint(0,300)
+    
+    burbuja(vec, i)
+    print("Vector de "+str(i)+" números:")
+    print(vec)
+    print
+    
+    pos = random.randint(0,i-1)
+    temp = vec[pos]
+    print(pos)
+    print(temp)
+
+    posicion = binaria(vec, 0, i, temp, c, cont)
+    
+    print
+    print("La posición del número "+str(temp)+" es: "+str(posicion))
+    print
+    
+    i += 10
+    y[cont] = c[cont+1]
+    cont += 1
+    
+
+print(x)
+print(y)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
